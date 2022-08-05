6ba19b0da36e80aecc34838686135c711acb5011
Miguel Lopez <hello@miguellopez.dev>
2022-08-04 20:36:07 -0700

    Remove unused const

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const logo = document.querySelector('.logo__image');
 const sideBar = document.querySelector('.sidebar');
-const style = getComputedStyle(sideBar);
 
 logo.addEventListener('click', function () {
 	sideBar.classList.toggle('sidebar-extend');
