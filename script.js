903bdee77025a3946f37b50df31b25fbcb442a9f
Miguel Lopez <hello@miguellopez.dev>
2022-08-04 20:33:05 -0700

    Fix sidebar

--- script.js
+++ script.js
@@ -3,9 +3,5 @@ const sideBar = document.querySelector('.sidebar');
 const style = getComputedStyle(sideBar);
 
 logo.addEventListener('click', function () {
-	if (sideBar.classList.width <= 60 + 'px') {
-		sideBar.classList.toggle('sidebar-extend');
-	}
+	sideBar.classList.toggle('sidebar-extend');
 });
-
-console.log(style.width);
